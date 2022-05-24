8c89f38c7a8861496152e1c714b6ebcdc82de84c
aisheulukhan <tu7hnsxs1@code.edu.az>
2022-05-24 13:44:37 +0400

    add

---
+++ TaskNest/TaskNest/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskNest.DAL;
+using TaskNest.Models;
+using TaskNest.ViewModels;
+
+namespace TaskNest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeVM homeVM = new HomeVM()
+            {
+                Carusels = await _context.Carusels.ToListAsync(),
+                Categories = await _context.Categories.ToListAsync(),
+                Products = await _context.Products.Include(p=>p.ProductImages).Include(p=>p.Category).Take(10).ToListAsync()
+            };
+            return View(homeVM);
+        }
+    }
+}
